f249ac88fefec50448fa0041a8152f6ed47010e1
kaushik <kaushik@kaushiks-MacBook-Pro.local>
2018-04-22 14:20:18 +0530

    Set A.C view constrain

---
+++ MisterMovers/AccountCell.h
@@ -0,0 +1,13 @@
+//
+//  AccountCell.h
+//  MisterMovers
+//
+//  Created by kaushik on 22/04/18.
+//  Copyright © 2018 jkinfoway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountCell : UITableViewCell
+
+@end
